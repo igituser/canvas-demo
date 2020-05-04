158d6292bf12f02395ef85a108847630722fadfa
Jianwei Yang <striveyang@outlook.com>
2020-05-04 14:25:09 +0800

    增加viewport视口设置，适配手机touch事件

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 autoSetCanvasSize();
-listenToMouse();
+listenToUser();
 
 
 var eraserEnabled = false;
@@ -34,44 +34,87 @@ function autoSetCanvasSize(){
 }
 
 
-function listenToMouse(){
+function listenToUser(){
     var useing = false;
     var lastPoint = {"x": undefined , "y": undefined }
-    
-    canvas.onmousedown = function(e){
-        useing = true;
-        var x = e.clientX;
-        var y = e.clientY;
-    
-        lastPoint = {"x": x , "y": y};
-    
-        // drawPoint(x,y);
-    
-        if(eraserEnabled){
-            ctx.clearRect(x-5, y-5, 10, 10);
-        }
-    
-    }
-    
-    canvas.onmousemove = function(e){
-        var x = e.clientX;
-        var y = e.clientY;
-        var newPoint = {"x":x, "y":y}
-    
-        if(!useing){ return }
-    
-        if(eraserEnabled){
-            ctx.clearRect(x-5, y-5, 10 ,10)
-        }else{
+
+
+    //*** 特性检测 */
+    // 判断设备支持触摸事件决定使用API
+    // 判断 ontouchstart 的值 undefined 不支持  null支持
+    // 'ontouchstar' in document.documentElement 是false 还有true
+    if(document.body.ontouchstart !== undefined && document.body.ontouchstart === null){
+        canvas.ontouchstart = function(e){
+            useing = true;
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+        
+            lastPoint = {"x": x , "y": y};
+        
             // drawPoint(x,y);
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-            lastPoint = newPoint;
+        
+            if(eraserEnabled){
+                ctx.clearRect(x-5, y-5, 10, 10);
+            }
         }
-    
-    }
-    
-    canvas.onmouseup = function(e){
-        useing = false;
+
+        canvas.ontouchmove = function(e){
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+            var newPoint = {"x":x, "y":y}
+        
+            if(!useing){ return }
+        
+            if(eraserEnabled){
+                ctx.clearRect(x-5, y-5, 10 ,10)
+            }else{
+                // drawPoint(x,y);
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint;
+            }
+        }
+
+        canvas.ontouchend = function(e){
+            console.log('end')
+        }
+    }else{
+
+        canvas.onmousedown = function(e){
+            useing = true;
+            var x = e.clientX;
+            var y = e.clientY;
+        
+            lastPoint = {"x": x , "y": y};
+        
+            // drawPoint(x,y);
+        
+            if(eraserEnabled){
+                ctx.clearRect(x-5, y-5, 10, 10);
+            }
+        
+        }
+        
+        canvas.onmousemove = function(e){
+            var x = e.clientX;
+            var y = e.clientY;
+            var newPoint = {"x":x, "y":y}
+        
+            if(!useing){ return }
+        
+            if(eraserEnabled){
+                ctx.clearRect(x-5, y-5, 10 ,10)
+            }else{
+                // drawPoint(x,y);
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint;
+            }
+        
+        }
+        
+        canvas.onmouseup = function(e){
+            useing = false;
+        }
+
     }
     
     function drawPoint(x, y){
@@ -88,3 +131,4 @@ function listenToMouse(){
         ctx.closePath();
     }
 }
+
